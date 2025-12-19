471977012c5ddba0a9b44c2ad61097455e7d626e
LeorierYoann <yoann.leorier@atos.net>
2025-12-19 09:15:32 +0000

    Création SG et VM

--- main.tf
+++ main.tf
@@ -16,6 +16,10 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "tls_private_key" "ssh" {
   algorithm = "ED25519"
 }
@@ -27,4 +31,55 @@ resource "aws_key_pair" "keypair1" {
   tags = {
     Name = "keypair1"
   }
+}
+
+resource "aws_instance" "otl" {
+  ami = "resolve:ssm:/aws/service/canonical/ubuntu/server/24.04/stable/current/amd64/hvm/ebs-gp3/ami-id"
+
+  instance_type = "t2.micro"
+
+  key_name = aws_key_pair.keypair1.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.sg1.id
+  ]
+
+  tags = {
+    Name = "otl"
+  }
+}
+
+resource "aws_security_group" "sg1" {
+  name        = "sg1"
+  description = "Security group pour nos webservers"
+  vpc_id      = data.aws_vpc.default.id
+
+  tags = {
+    Name = "sg1"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+  security_group_id = aws_security_group.sg1.id
+
+  ip_protocol = "tcp"
+  from_port   = 22
+  to_port     = 22
+  cidr_ipv4   = "0.0.0.0/0"
+
+  description = "Autoriser SSH depuis le monde entier"
+}
+
+resource "aws_vpc_security_group_egress_rule" "all_out" {
+  security_group_id = aws_security_group.sg1.id
+
+  ip_protocol = "-1"
+  cidr_ipv4   = "0.0.0.0/0"
+
+  description = "Autoriser tout le trafic sortant"
+}
+
+output "otl_public_ip" {
+  value       = aws_instance.otl.public_ip
+  description = "IP publique de l'instance otl"
 }
