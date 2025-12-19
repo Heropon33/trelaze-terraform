7e666c0bcf354a71ba20fa69dcfc38cac40730da
LeorierYoann <yoann.leorier@atos.net>
2025-12-19 08:43:05 +0000

    feat: Création première keypair

--- main.tf
+++ main.tf
@@ -10,3 +10,16 @@ terraform {
 provider "aws" {
   region = "eu-north-1"
 }
+
+resource "tls_private_key" "ssh" {
+  algorithm = "ED25519"
+}
+
+resource "aws_key_pair" "keypair1" {
+  key_name   = "keypair1"
+  public_key = tls_private_key.ssh.public_key_openssh
+
+  tags = {
+    Name = "keypair1"
+  }
+}
